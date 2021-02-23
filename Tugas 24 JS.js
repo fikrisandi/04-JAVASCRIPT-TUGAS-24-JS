4c6a38765eb2e2b47abfb49e78fe9822c12721f0
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-02-23 14:04:11 +0700

    Tugas 24 JS

---
+++ Tugas 24 JS.js
@@ -0,0 +1,9 @@
+var arr = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log("Sebelumnya :", arr.join(","));
+// Sebelum di urutkan
+arr = arr.sort();
+console.log("Ascending : ", arr.join(","));
+// sesudah di urutkan dari kecil ke besar
+arr = arr.reverse();
+console.log("Descending :", arr.join(","));
+// sesudah di urutkan dari besar ke kecil
